e2eef5770a1d856b3a3f3918ac62d92c9368ebd0
Nickolas Burr <nickolasburr@gmail.com>
2025-03-29 01:24:43 -0400

    Add types.h, str.h headers

---
+++ include/types.h
@@ -0,0 +1,15 @@
+/**
+ * types.h
+ *
+ * Copyright (C) 2025 Nickolas Burr <nickolasburr@gmail.com>
+ */
+
+#ifndef XDEF_TYPES_H
+#define XDEF_TYPES_H
+
+#define IS_NULL(ptr) ((ptr) == ((void *) 0))
+
+int is_alphanum(char *);
+int is_numeric(char *);
+
+#endif /* XDEF_TYPES_H */
---
+++ include/types/str.h
@@ -0,0 +1,16 @@
+/**
+ * str.h
+ *
+ * Copyright (C) 2025 Nickolas Burr <nickolasburr@gmail.com>
+ */
+
+#ifndef XDEF_TYPES_STR_H
+#define XDEF_TYPES_STR_H
+
+#define CRLF "\r\n"
+#define NUL_BYTE 1
+
+char *concat(const char *, const char *);
+char *copy(const char *);
+
+#endif /* XDEF_TYPES_STR_H */
