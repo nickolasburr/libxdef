4139ed395a3a1dd3ce7d7d0d8f5666ec6b6a00fd
Nickolas Burr <nickolasburr@gmail.com>
2025-04-04 00:18:07 -0400

    Update decls in mem.h

--- include/mem.h
+++ include/mem.h
@@ -11,16 +11,16 @@
 
 extern const Except_T Mem_Failed;
 
-extern void *Mem_alloc(long, const char *, int);
-extern void *Mem_calloc(long, long, const char *, int);
-extern void Mem_free(void *, const char *, int);
-extern void *Mem_resize(void *, long, const char *, int);
+extern void *MEM_alloc(long, const char *, int);
+extern void *MEM_calloc(long, long, const char *, int);
+extern void MEM_free(void *, const char *, int);
+extern void *MEM_resize(void *, long, const char *, int);
 
-#define ALLOC(nbytes)         Mem_alloc((nbytes), __FILE__, __LINE__)
-#define CALLOC(count, nbytes) Mem_calloc((count), (nbytes), __FILE__, __LINE__)
+#define ALLOC(nbytes)         MEM_alloc((nbytes), __FILE__, __LINE__)
+#define CALLOC(count, nbytes) MEM_calloc((count), (nbytes), __FILE__, __LINE__)
 #define NEW(p)                ((p) = ALLOC((long) sizeof *(p)))
 #define NEW0(p)               ((p) = CALLOC(1, (long) sizeof *(p)))
-#define FREE(ptr)             ((void)(Mem_free((ptr), __FILE__, __LINE__), (ptr) = 0))
-#define RESIZE(ptr, nbytes)   ((ptr) = Mem_resize((ptr), (nbytes), __FILE__, __LINE__))
+#define FREE(ptr)             ((void)(MEM_free((ptr), __FILE__, __LINE__), (ptr) = 0))
+#define RESIZE(ptr, nbytes)   ((ptr) = MEM_resize((ptr), (nbytes), __FILE__, __LINE__))
 
 #endif /* XDEF_MEM_H */
